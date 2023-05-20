3bc1336427e1241c0095ebbd3f7a531f1da78d88
shakilkhan2 <dev.shakilkhan@gmail.com>
2023-05-20 20:04:07 +0600

    slider fixed

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -7,11 +7,14 @@ import Categories from "../categories/Categories";
 const Home = () => {
   return (
     <div>
-      <Slider />
+      <div>
+        <Slider></Slider>
+      </div>
       <Gallery />
-      <h1 className="text-center text-4xl font-bold text-sky-600 mt-8 mb-4 ">Shop by Categories</h1>
-    <Categories/>
-      
+      <h1 className="text-center text-4xl font-bold text-sky-600 mt-8 mb-4 ">
+        Shop by Categories
+      </h1>
+      <Categories />
     </div>
   );
 };
--- src/pages/slider/Slider.jsx
+++ src/pages/slider/Slider.jsx
@@ -3,13 +3,13 @@ import React from "react";
 const Slider = () => {
   return (
     <div>
-      <div className="carousel  h-1/2">
+      <div className="carousel  h-[100vh]">
         <div id="slide1" className="carousel-item relative w-full ">
           <img
             src="https://i.ibb.co/TwMRKk3/slide1.jpg"
-            className="w-full object-cover h-1/2"
+            className="w-full object-cover h-[100vh]"
           />
-          <div className="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/4">
+          <div className="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2">
             <a href="#slide4" className="btn btn-circle">
               ❮
             </a>
@@ -21,9 +21,9 @@ const Slider = () => {
         <div id="slide2" className="carousel-item relative w-full">
           <img
             src="https://i.ibb.co/CJZPMzQ/slide2.jpg"
-            className="w-full object-cover h-1/2"
+            className="w-full object-cover h-[100vh]"
           />
-          <div className="absolute flex justify-between transform -translate-y-1/2 left-5 right-5  top-1/4">
+          <div className="absolute flex justify-between transform -translate-y-1/2 left-5 right-5  top-1/2">
             <a href="#slide1" className="btn btn-circle">
               ❮
             </a>
@@ -35,9 +35,9 @@ const Slider = () => {
         <div id="slide3" className="carousel-item relative w-full">
           <img
             src="https://i.ibb.co/TwMRKk3/slide1.jpg"
-            className="w-full object-cover h-1/2"
+            className="w-full object-cover h-[100vh]"
           />
-          <div className="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/4">
+          <div className="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2">
             <a href="#slide2" className="btn btn-circle">
               ❮
             </a>
@@ -49,9 +49,9 @@ const Slider = () => {
         <div id="slide4" className="carousel-item relative w-full">
           <img
             src="https://i.ibb.co/CJZPMzQ/slide2.jpg"
-            className="w-full object-cover h-1/2"
+            className="w-full object-cover h-[100vh]"
           />
-          <div className="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/4">
+          <div className="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2">
             <a href="#slide3" className="btn btn-circle">
               ❮
             </a>
